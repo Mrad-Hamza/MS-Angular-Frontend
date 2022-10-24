4739e8bb151cc19c9d12e9832375142d4e1d2aa3
MohsniNour <nourelhouda.mohsni@esprit.tn>
2022-10-24 12:40:31 +0100

    add user connection

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,11 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
-@Injectable()
-export class AuthService {
-    constructor() {}
+const URI = "http://localhost:8090/user";
 
-    getAuth$(): Observable<{}> {
-        return of({});
+@Injectable({
+    providedIn:'root'
+})
+export class AuthService {
+    username!:string;
+    password!:string;
+      
+    constructor(private http:HttpClient) {}
+
+    public login(username:string,password:string){
+        const data={}
+        const headers=new HttpHeaders({Authorization:'Basic'+btoa(username+":"+password)})
+        return this.http.post(URI + "/signin",{headers,responseType:'text'as 'json'} )
     }
-}
+    }
+
+    // getAuth$(): Observable<{}> {
+    //     return of({});
+    // }
